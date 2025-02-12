ab0fc7521f4eff3768af4c509e0a0630477114c9
Heath Stewart <heaths@outlook.com>
2025-02-12 01:36:19 -0800

    Implement list command (#6)
    * Attempt with try_fold()
    
    * Implement list command with try_stream!{}

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,28 @@
 
 mod error;
 
+use async_stream::try_stream;
 use azure_security_keyvault_secrets::{
     models::{SecretBundle, SecretItem},
     SecretClient,
 };
 pub use error::*;
-use futures::{stream, TryStream};
+use futures::{Stream, StreamExt};
 use tracing::Level;
 
 #[tracing::instrument(level = Level::INFO, skip(client), fields(vault = %client.endpoint()))]
-pub async fn list_secrets(client: &SecretClient) -> impl TryStream {
-    stream::empty::<Result<SecretItem>>()
+pub fn list_secrets(client: &SecretClient) -> impl Stream<Item = Result<SecretItem>> + '_ {
+    try_stream! {
+        let mut pager = client.get_secrets(None)?;
+        while let Some(page) = pager.next().await {
+            let result = page?.into_body().await?;
+            if let Some(secrets) = result.value {
+                for secret in secrets {
+                    yield secret;
+                }
+            }
+        }
+    }
 }
 
 #[tracing::instrument(level = Level::INFO, skip(client), fields(vault = %client.endpoint()), err)]
