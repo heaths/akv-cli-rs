5f0a0e369588be38d1ebb750ce331d4ff647a4e9
Heath Stewart <heaths@outlook.com>
2025-02-12 08:19:11 -0800

    Refactor out command handlers

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 // Copyright 2025 Heath Stewart.
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
+use akv_cli::Result;
 use akv_cli::{get_secret, list_secrets};
-use azure_core::{date::OffsetDateTime, Url};
+use azure_core::{credentials::TokenCredential, date::OffsetDateTime, Url};
 use azure_identity::DefaultAzureCredential;
 use azure_security_keyvault_secrets::{ResourceExt, ResourceId, SecretClient};
 use clap::{Parser, Subcommand};
 use futures::TryStreamExt;
 use prettytable::{format, row, Table};
-use std::pin::pin;
+use std::{pin::pin, sync::Arc};
 use timeago::Formatter;
 use tracing::level_filters::LevelFilter;
 use tracing_subscriber::{fmt::format::FmtSpan, EnvFilter};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let args = Args::parse();
 
     let verbosity = match args.verbose {
@@ -34,41 +35,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let credentials = DefaultAzureCredential::new()?;
     match args.command {
-        Commands::List { vault } => {
-            let client = SecretClient::new(vault.as_str(), credentials.clone(), None)?;
-            let mut pager = pin!(list_secrets(&client));
-
-            let mut table = Table::new();
-            table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
-            table.set_titles(row!["NAME", "ID", "EDITED", "TYPE"]);
-
-            let now = OffsetDateTime::now_utc();
-            let formatter = Formatter::new();
-            while let Some(secret) = pager.try_next().await? {
-                let resource: ResourceId = secret.resource_id()?;
-                let edited = secret
-                    .attributes
-                    .and_then(|attrs| attrs.updated)
-                    .map(|time| now - time)
-                    .map(into_std_duration)
-                    .map_or_else(String::new, |d| formatter.convert(d));
-                let r#type = secret.content_type.unwrap_or_default();
-                table.add_row(row![Fg -> resource.name, resource.source_id, edited, r#type]);
-            }
-            // cspell:ignore printstd
-            table.printstd();
-        }
-        Commands::Read { id } => {
-            let id: ResourceId = id.try_into()?;
-            let client = SecretClient::new(&id.vault_url, credentials.clone(), None)?;
-            let secret = get_secret(&client, id.name.as_ref(), id.version.as_deref()).await?;
-            if let Some(value) = secret.value {
-                println!("{value}");
-            }
-        }
+        Commands::List { vault } => list(credentials.clone(), vault).await,
+        Commands::Read { id } => read(credentials.clone(), id).await,
     }
-
-    Ok(())
 }
 
 #[derive(Debug, Parser)]
@@ -95,6 +64,43 @@ enum Commands {
         /// The URL to a secret in Azure Key Vault e.g., "https://my-vault.vault.azure.net/secrets/my-secret".
         id: Url,
     },
+}
+
+async fn list(credentials: Arc<dyn TokenCredential>, vault: Url) -> Result<()> {
+    let client = SecretClient::new(vault.as_str(), credentials, None)?;
+    let mut pager = pin!(list_secrets(&client));
+
+    let mut table = Table::new();
+    table.set_format(*format::consts::FORMAT_NO_BORDER_LINE_SEPARATOR);
+    table.set_titles(row!["NAME", "ID", "EDITED", "TYPE"]);
+
+    let now = OffsetDateTime::now_utc();
+    let formatter = Formatter::new();
+    while let Some(secret) = pager.try_next().await? {
+        let resource: ResourceId = secret.resource_id()?;
+        let edited = secret
+            .attributes
+            .and_then(|attrs| attrs.updated)
+            .map(|time| now - time)
+            .map(into_std_duration)
+            .map_or_else(String::new, |d| formatter.convert(d));
+        let r#type = secret.content_type.unwrap_or_default();
+        table.add_row(row![Fg -> resource.name, resource.source_id, edited, r#type]);
+    }
+    // cspell:ignore printstd
+    table.printstd();
+
+    Ok(())
+}
+async fn read(credentials: Arc<dyn TokenCredential>, id: Url) -> Result<()> {
+    let id: ResourceId = id.try_into()?;
+    let client = SecretClient::new(&id.vault_url, credentials.clone(), None)?;
+    let secret = get_secret(&client, id.name.as_ref(), id.version.as_deref()).await?;
+    if let Some(value) = secret.value {
+        println!("{value}");
+    }
+
+    Ok(())
 }
 
 fn into_std_duration(d: time::Duration) -> std::time::Duration {
