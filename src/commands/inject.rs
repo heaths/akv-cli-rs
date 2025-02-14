5d80c62c213ce56617c0c0da54d417a6d1e283b9
Heath Stewart <heaths@microsoft.com>
2025-02-14 13:02:59 -0800

    Improve missing env var error (#17)

--- src/commands/inject.rs
+++ src/commands/inject.rs
@@ -60,18 +60,6 @@ impl Args {
             }
         };
 
-        let mut output: Box<dyn Write> = match &self.out_file {
-            Some(out_file) => Box::new(
-                fs::OpenOptions::new()
-                    .create(true)
-                    .write(true)
-                    .truncate(true)
-                    .create_new(!self.force)
-                    .open(out_file)?,
-            ),
-            _ => Box::new(io::stdout()),
-        };
-
         let credentials = DefaultAzureCredential::new()?;
         let mut cache = ClientCache::new();
         if let Some(vault) = self.vault.as_ref() {
@@ -82,7 +70,8 @@ impl Args {
             )?))?;
         };
 
-        replace_expressions(&input, &mut output, |expr| {
+        let mut buf = Vec::new();
+        replace_expressions(&input, &mut buf, |expr| {
             let mut cache = cache.clone();
             let credentials = credentials.clone();
 
@@ -91,7 +80,15 @@ impl Args {
 
                 let id = replace_vars(expr, |var| {
                     tracing::debug!("replacing variable ${var}");
-                    env::var(var).map_err(Into::into)
+                    env::var(var).map_err(|err| {
+                        if let env::VarError::NotPresent = err {
+                            return akv_cli::Error::with_message_fn(ErrorKind::Other, || {
+                                format!("environment variable ${var} not found")
+                            });
+                        }
+
+                        err.into()
+                    })
                 })?;
 
                 tracing::debug!("reading secret {id}");
@@ -113,6 +110,22 @@ impl Args {
             }
             .boxed()
         })
-        .await
+        .await?;
+
+        let mut output: Box<dyn Write> = match &self.out_file {
+            Some(out_file) => Box::new(
+                fs::OpenOptions::new()
+                    .create(true)
+                    .write(true)
+                    .truncate(true)
+                    .create_new(!self.force)
+                    .open(out_file)?,
+            ),
+            _ => Box::new(io::stdout()),
+        };
+
+        io::copy(&mut buf.as_slice(), output.as_mut())
+            .map(|_| ())
+            .map_err(Into::into)
     }
 }
