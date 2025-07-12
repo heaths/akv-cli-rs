83355ea3b03d9778d8a3fd9e8434c1db573e4f9d
Heath Stewart <heaths@outlook.com>
2025-07-12 00:57:43 -0700

    Support wildcards for printenv example

--- examples/printenv.rs
+++ examples/printenv.rs
@@ -2,13 +2,14 @@
 // Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 use anstyle::{AnsiColor, Style};
-use clap::{ColorChoice, Parser};
+use clap::{builder::TypedValueParser, ColorChoice, Parser};
 use std::{
     env,
     io::{self, Write},
     time::Duration,
 };
 use tokio::time::sleep;
+use wildcard::Wildcard;
 
 const GREEN: Style = Style::new().fg_color(Some(anstyle::Color::Ansi(AnsiColor::Green)));
 const RED: Style = Style::new().fg_color(Some(anstyle::Color::Ansi(AnsiColor::Red)));
@@ -16,26 +17,22 @@ const RED: Style = Style::new().fg_color(Some(anstyle::Color::Ansi(AnsiColor::Re
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let mut vars: Vec<(String, String)> = if args.vars.is_empty() {
-        env::vars_os()
-            .filter_map(|(k, v)| {
-                let key = k.into_string().ok()?;
-                if args.all || !key.starts_with("_") {
-                    return Some((key, v.into_string().ok()?));
-                }
-                None
-            })
-            .collect()
-    } else {
-        let mut vars = Vec::new();
-        for k in &args.vars {
-            let Some(v) = env::var(k).ok() else {
-                continue;
-            };
-            vars.push((k.clone(), v));
-        }
-        vars
-    };
+    let mut vars: Vec<(String, String)> = env::vars_os()
+        .filter_map(|(k, v)| {
+            let key = k.as_os_str().to_str()?;
+            // First determine if we should include "private" variables.
+            if !args.all && key.starts_with("_") {
+                return None;
+            }
+
+            // Second determine if there are any variables or patterns to match.
+            if !args.is_match(key) {
+                return None;
+            }
+
+            Some((key.to_owned(), v.into_string().ok()?))
+        })
+        .collect();
 
     // Sort for easy comparison to expected output.
     vars.sort_by(|a, b| a.0.cmp(&b.0));
@@ -74,9 +71,9 @@ struct Args {
     #[arg(long)]
     delay: Option<u64>,
 
-    /// Optional variable names to print.
-    #[arg(value_name = "VARIABLES", trailing_var_arg = true)]
-    vars: Vec<String>,
+    /// Optional variable names to print. These may contain wildcard symbols `*` and `?`.
+    #[arg(value_name = "VARIABLES", value_parser = WildcardValueParser, trailing_var_arg = true)]
+    vars: Vec<Wildcard<'static>>,
 }
 
 impl Args {
@@ -86,5 +83,36 @@ impl Args {
             ColorChoice::Auto => anstream::ColorChoice::Auto,
             ColorChoice::Never => anstream::ColorChoice::Never,
         }
+    }
+
+    fn is_match(&self, s: impl AsRef<str>) -> bool {
+        if self.vars.is_empty() {
+            return true;
+        }
+
+        for var in &self.vars {
+            if var.is_match(s.as_ref().as_bytes()) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+#[derive(Clone)]
+struct WildcardValueParser;
+
+impl TypedValueParser for WildcardValueParser {
+    type Value = Wildcard<'static>;
+    fn parse_ref(
+        &self,
+        _cmd: &clap::Command,
+        _arg: Option<&clap::Arg>,
+        value: &std::ffi::OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        let bytes = value.as_encoded_bytes().to_vec();
+        Wildcard::from_owned(bytes)
+            .map_err(|_| clap::Error::new(clap::error::ErrorKind::InvalidValue))
     }
 }
