45f3586fd481a316d0dd98c72168927c36f6e4f0
Heath Stewart <heaths@outlook.com>
2026-04-07 22:53:51 -0700

    Require key version or full key URI for encrypt
    Azure Key Vault SDK for Rust now requires key version for cryptography APIs to prevent data lock-out.

--- examples/setup.sh
+++ examples/setup.sh
@@ -2,7 +2,8 @@
 # Licensed under the MIT License. See LICENSE.txt in the project root for license information.
 
 AZURE_KEYVAULT_URL="$(azd env get-value AZURE_KEYVAULT_URL)"
+AZURE_KEYVAULT_DEK_URL="$(azd env get-value AZURE_KEYVAULT_DEK_URL)"
 export X_SECRET_1="${AZURE_KEYVAULT_URL%/}/secrets/secret-1"
 export X_SECRET_2="${AZURE_KEYVAULT_URL%/}/secrets/secret-2"
-export X_JWE=$(cargo run -- encrypt --vault "${AZURE_KEYVAULT_URL}" --name 'dek' 'Hello, world!')
-export X_JWE_BIN=$(dd if=/dev/random bs=4 count=1 2>/dev/null | cargo run -- encrypt --vault "${AZURE_KEYVAULT_URL}" --name 'dek' --in-file '-')
+export X_JWE=$(cargo run -- encrypt "${AZURE_KEYVAULT_DEK_URL}" 'Hello, world!')
+export X_JWE_BIN=$(dd if=/dev/random bs=4 count=32 2>/dev/null | cargo run -- encrypt "${AZURE_KEYVAULT_DEK_URL}" --in-file '-')
